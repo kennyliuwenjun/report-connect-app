bc86864847fef0049dc629ef6d0445aa84c90cbd
Wenjun Kenny Liu <kennyliu1990110@hotmail.com>
2018-07-19 12:04:49 +1000

    readme setup

--- README.md
+++ README.md
@@ -1,24 +1,25 @@
-# README
+# ReportConnect App
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+live link: https://report-connect.herokuapp.com/
 
-Things you may want to cover:
+## App Main Features
+- Sign in, Sign out, Sign up for every individual users
+- Use chart to display dataset
+- A user can invite another user to watch or admin their businesses
 
-* Ruby version
+## Objectives
+- App should have at least 3 models. Users, Admins, Businesses and more.
+- Handles invalid data. Forms in your application should validate data and handle incorrect inputs.
+- Validate sign up information, verify valid email addresses and secure passwords.
+- Use FusionCharts Gem for displaying of data
+- Heroku. Deploy your code to Heroku.
 
-* System dependencies
+## Built With
+- HTML
+- CSS
+- Ruby on Rails
+- Bootstrap
 
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+## Screenshots
+![alt text](screenshots/table_example.jpg "An example of table")
+![alt text](screenshots/chart_example.jpg "An example of chart")
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,13 +18,17 @@ a1 = Admin.create :user_id => u1.id, :busniess_id => b1.id
 a2 = Admin.create :user_id => u1.id, :busniess_id => b2.id
 
 Report.destroy_all
-r1 = Report.create :sales => 19.955, :date => DateTime.new(2017,12,1)
-r2 = Report.create :sales => 0.1111, :date => DateTime.new(2017,11,23)
-r3 = Report.create :sales => 93.444, :expenses => 0.11, :date => DateTime.new(2017,10,1), :comments => 'nothing to say'
-r4 = Report.create :sales => 4, :expenses => 0.11, :date => DateTime.new(2017,11,1)
-r5 = Report.create :sales => 1, :expenses => 0.9, :date => DateTime.new(2017,11,2)
-r6 = Report.create :sales => 2, :expenses => 0.099, :date => DateTime.new(2017,11,3), :comments => 'say'
-r7 = Report.create :sales => 3, :expenses => 0.9999, :date => DateTime.new(2017,11,4), :comments => 'nothing'
+r1 = Report.create :sales => 1000, :expenses => 200.11, :date => DateTime.new(2017,11,1)
+r2 = Report.create :sales => 900, :expenses => 300.11, :date => DateTime.new(2017,11,2)
+r3 = Report.create :sales => 800.56, :expenses => 2000, :date => DateTime.new(2017,11,4), :comments => 'employee salary pay'
+r4 = Report.create :sales => 700, :expenses => 200.55, :date => DateTime.new(2017,11,14)
+r5 = Report.create :expenses => 100, :date => DateTime.new(2017,11,23), :comments => 'holiday'
+r6 = Report.create :sales => 900, :expenses => 2222.22, :date => DateTime.new(2018,7,1), :comments => 'electricity bill'
+r8 = Report.create :sales => 900, :expenses => 50, :date => DateTime.new(2018,7,2)
+r9 = Report.create :sales => 900, :expenses => 10, :date => DateTime.new(2018,7,3)
+r10 = Report.create :sales => 700, :expenses => 100, :date => DateTime.new(2018,7,4)
+r11 = Report.create :sales => 789, :expenses => 22.22, :date => DateTime.new(2018,7,5)
+r12 = Report.create :sales => 1000, :expenses => 2221.2, :date => DateTime.new(2018,7,19), :comments => 'purchases'
 
-b1.reports << r1 << r2 << r3 << r4 << r5 << r6 << r7
+b1.reports << r1 << r2 << r3 << r4 << r5 << r6 << r8 << r9 << r10 << r11 << r12
 u1.busniesses << b3
